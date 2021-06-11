869d22784b71bd17be85cab146beb4d81c2ad26c
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-06-11 15:41:31 +0530

    csv file readingg

--- read.py
+++ read.py
@@ -1,24 +1,46 @@
+#from dateutil.parser import parse
+import datetime
 import csv
-genere=[]
+from typing import Counter
+
 def load_csv():
     with open('netflixoriginals.csv', 'r') as file:
-
         reader = csv.reader(file)
-
-    
+        year_list=[]
+        genre_list=[]       
         for i in reader:
-            genere.append(i[1])
-            print(genere)
+
+            premier_data=i[2]            
+            year_p=premier_data[-4:] 
+            genre_data=i[1]           
+
+            genre_list.append(genre_data)
+            year_list.append(year_p)
             
+    return year_list,genre_list
+          
+year,genre=load_csv()  
+#print(year,genre)
 
-            
 
-           
+def filter_genre_year(year,genre):
+    #load_csv()
+    for i in year:
+        #print(i)
+        if i=='2020': 
+            genre_count=[ [l, genre.count(l)] for l in set(genre)] 
+        
+            #print ([ [l, genre.count(l)] for l in set(genre)]) 
+    return genre_count
 
-def filter_genre_year(genere):
-    load_csv()
-    genere_count=
     
-	#return genre_count
-filter_genre_year(genere)
+        
+       
+filter_genre_year(year,genre)
+print(filter_genre_year(year,genre))
+
+       
+        
+
+    
 
